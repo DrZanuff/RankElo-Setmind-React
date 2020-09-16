7b379cb2ca1c14ed9e72ea85e90306243e8a8766
DrZanuff <ricardo.machado.nwi@gmail.com>
2020-09-16 15:40:24 -0300

    Doc page

---
+++ docs/precache-manifest.5dd281e28deb833a52e49fa319e776cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81541f7e7c71915d5ea4441fd1734702",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4292c823041838c9665a",
+    "url": "/static/css/main.4edd1abc.chunk.css"
+  },
+  {
+    "revision": "b1e29df9ee6e4b813aa4",
+    "url": "/static/js/2.d0a23da7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d0a23da7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4292c823041838c9665a",
+    "url": "/static/js/main.d376f92e.chunk.js"
+  },
+  {
+    "revision": "a939b06b78b95d51848f",
+    "url": "/static/js/runtime-main.43a00307.js"
+  }
+]);
