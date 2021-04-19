b4fd7fdf45a6a95e34c24a47285459443766d274
DrZanuff <ricardo.machado.nwi@gmail.com>
2021-04-19 16:55:35 -0300

    Changed Font Size

--- script.jsx
+++ script.jsx
@@ -264,7 +264,7 @@ function formatEntrySize(len){
     let size = len
     const clampNumber = (num, a, b) => Math.max(Math.min(num, Math.max(a, b)), Math.min(a, b));
 
-    let styles = {height:(total/size) , fontSize : clampNumber( (total/size) * 40 , 30 , 50)  }
+    let styles = {height:(total/size) , fontSize : clampNumber( (total/size) * 20 , 8 , 50)  }
     return styles 
 }
 
